1097a0afb2751c74bb1755924a9d753071bd8975
Padi Amu <padiamu@gmail.com>
2025-01-20 14:43:41 +0000

    first commit

---
+++ src/main/java/co/terydin/logging/SampleLoggingServiceApplication.java
@@ -0,0 +1,13 @@
+package co.terydin.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleLoggingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleLoggingServiceApplication.class, args);
+	}
+
+}
